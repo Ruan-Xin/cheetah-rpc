2bd5a2e0ab1dcb1b92a445f6a49b1f76b46a2c66
Ruan-Xin <415131215@qq.com>
2017-12-03 17:02:01 +0800

    add alias

--- src/main/java/com/roy/cheetah/rpc/client/SimpleClientRemoteProxy.java
+++ src/main/java/com/roy/cheetah/rpc/client/SimpleClientRemoteProxy.java
@@ -2,11 +2,14 @@ package com.roy.cheetah.rpc.client;
 
 import com.roy.cheetah.rpc.RemoteCall;
 import com.roy.cheetah.rpc.RemoteExecutor;
+import com.roy.cheetah.rpc.RpcContext;
 import com.roy.cheetah.rpc.RpcService;
 import com.roy.cheetah.rpc.utils.RpcUtils;
+import com.roy.cheetah.rpc.utils.XAliasUtils;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -28,11 +31,19 @@ public class SimpleClientRemoteProxy implements InvocationHandler, RpcService {
     private String application;
 
     public void startService() {
-
+        remoteExecutor.startService();
     }
 
     public void stopService() {
+        remoteExecutor.startService();
+    }
 
+    public void setApplication(String application) {
+        this.application = application;
+    }
+
+    public String getApplication() {
+        return application;
     }
 
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
@@ -54,7 +65,49 @@ public class SimpleClientRemoteProxy implements InvocationHandler, RpcService {
         }
         call.setGroup(group);
 
+        //上下文 attachment支持
+        Map<String, Object> attachment = RpcContext.getContext().getAttachment();
+        call.setAttachment(attachment);
 
-        return null;
+        call.getAttachment().put("Application", application);
+
+        if (method.getReturnType() == void.class) {
+            remoteExecutor.oneWay(call);
+            return null;
+        }
+        return remoteExecutor.invoke(call);
+    }
+
+    public RemoteExecutor getRemoteExecutor() {
+        return remoteExecutor;
+    }
+
+    public void setRemoteExecutor(RemoteExecutor remoteExecutor) {
+        this.remoteExecutor = remoteExecutor;
+    }
+
+    public <Iface> Iface registerRemote(Class<Iface> remote) {
+        return registerRemote(remote, RpcUtils.DEFAULT_VERSION);
+    }
+
+    public <Iface> Iface registerRemote(Class<Iface> remote, String version) {
+        return registerRemote(remote, version, RpcUtils.DEFAULT_GROUP);
+    }
+
+    public <Iface> Iface registerRemote(Class<Iface> remote, String version, String group) {
+        Iface result = (Iface) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[]{remote}, this);
+        if (version == null) {
+            version = RpcUtils.DEFAULT_VERSION;
+        }
+        versionCache.put(remote, version);
+
+        if (group == null) {
+            group = RpcUtils.DEFAULT_GROUP;
+        }
+
+        XAliasUtils.addServiceRefType(remote);
+
+        groupCache.put(remote, group);
+        return result;
     }
 }
